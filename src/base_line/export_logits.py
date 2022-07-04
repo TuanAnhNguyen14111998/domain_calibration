cebd0a577afad804518f4346c543e25edb1a8e08
TuanAnhNguyen14111998 <tuananh14111998@gmail.com>
2022-07-05 06:29:05 +0700

    code for export logits with config yaml

--- src/base_line/export_logits.py
+++ src/base_line/export_logits.py
@@ -1,3 +1,4 @@
+from threading import main_thread
 import torch
 import albumentations as A
 import pandas as pd
@@ -6,6 +7,7 @@ import time
 import copy
 import glob
 import numpy as np
+import yaml
 
 import os
 import sys
@@ -25,6 +27,46 @@ from src.base_line.restnet34 import initialize_model
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
+def load_from_yaml(fname):
+    with open(fname, encoding='utf-8') as f:
+        base_config = yaml.safe_load(f)
+    return base_config
+
+def get_agument(augment_name):
+    list_augment = []
+    for augment in augment_name:
+        if augment == "CenterCrop":
+            list_augment.append(
+                A.CenterCrop(width=224, height=224),
+            )
+        if augment == "Flip":
+            list_augment.append(
+                A.Flip(always_apply=False, p=0.5)
+            )
+        if augment == "Blur":
+            list_augment.append(
+                A.Blur(always_apply=False, p=0.5, blur_limit=(3, 7))
+            )
+        if augment == "GaussNoise":
+            list_augment.append(
+                A.GaussNoise(always_apply=False, p=0.5, var_limit=(10.0, 50.0))
+            )
+        if augment == "RandomBrightness":
+            list_augment.append(
+                A.RandomBrightness(
+                    always_apply=False, p=0.5, 
+                    limit=(-0.20000000298023224, 0.20000000298023224))
+            )
+        if augment == "Normalize":
+            list_augment.append(
+                A.Normalize(
+                    mean=(0.485, 0.456, 0.406), 
+                    std=(0.229, 0.224, 0.225)
+                )
+            )
+
+    return A.Compose(list_augment) 
+
 def default_collate(batch):
     batch = list(filter(lambda x: x is not None, batch))
     images = torch.stack([torch.tensor(item[0]) for item in batch])
@@ -36,11 +78,15 @@ def default_collate(batch):
 
 def export_logits(
     model, dataloaders, 
-    folder_save="./experiments/", 
-    k_fold=0):
+    export_main_domain=True):
     
     df_finish_pred = pd.DataFrame()
-    for phase in ['train', 'val']:
+    if export_main_domain:
+        phases = ['train', 'val']
+    else:
+        phases = ['val']
+    
+    for phase in phases:
         model.eval()
         for inputs, labels, records in tqdm(dataloaders[phase]):
             inputs = inputs.to(device)
@@ -56,54 +102,33 @@ def export_logits(
 
 
 if __name__ == "__main__":
-    config_params = {
-        "dataset_name": ["Office-Home"],
-        "input_size": 256,
-        "batch_size": 132,
-        "num_workers": 4,
-        "learning_rate": 0.001,
-        "beta": (0.9,0.999),
-        "number_epochs": 120,
-    }
-    
+    config_params = load_from_yaml("./configs/exp.yaml")
 
     for dataset_name in config_params["dataset_name"]:
         if dataset_name == "Office-Home":
             path_information =\
-                f"/vinbrain/anhng/domain_adaptation/datasets/{dataset_name}/OfficeHomeDataset_10072016/information/"
+                f"{config_params['path_data']}/{dataset_name}/OfficeHomeDataset_10072016/information/"
+        elif dataset_name == "Bing-Caltech":
+            path_information =\
+                f"{config_params['path_data']}/{dataset_name}/information/"
         else:
             path_information =\
-                f"/vinbrain/anhng/domain_adaptation/datasets/{dataset_name}/information/"
+                f"{config_params['path_data']}/{dataset_name}/information/"
 
         all_csv_domain = glob.glob(path_information + "/*.csv")
-
         for path_csv in all_csv_domain:
-            domain_name = path_csv.split("/")[-1].replace("_kfold.csv", "")
+            main_domain = path_csv.split("/")[-1].replace("_kfold.csv", "")
 
-            print(f"Running on {dataset_name} with domain: {domain_name} .... ")
+            print(f"Running on {dataset_name} with domain: {main_domain} .... ")
             path_weight_save =\
-                f"/vinbrain/anhng/domain_adaptation/experiments/{dataset_name}/{domain_name}/"
+                f"/vinbrain/anhng/domain_adaptation/experiments/{dataset_name}/{main_domain}/"
 
             if not os.path.isdir(path_weight_save):
                 os.makedirs(path_weight_save)
 
             data_transforms = {
-                'train': A.Compose([
-                    A.Resize(width=224, height=224),
-                    A.CenterCrop(width=224, height=224),
-                    A.Normalize(
-                        mean=(0.485, 0.456, 0.406), 
-                        std=(0.229, 0.224, 0.225)
-                    )
-                ]),
-                'val': A.Compose([
-                    A.Resize(width=224, height=224),
-                    A.CenterCrop(width=224, height=224),
-                    A.Normalize(
-                        mean=(0.485, 0.456, 0.406), 
-                        std=(0.229, 0.224, 0.225)
-                    )
-                ])
+                'train': get_agument(config_params["train_augment"]),
+                'val': get_agument(config_params["val_augment"])
             }
 
             print("Initializing Datasets and Dataloaders...")
@@ -120,66 +145,77 @@ if __name__ == "__main__":
             k_fold = 3
             with pd.ExcelWriter(f'{path_weight_save}/resnet_34_kfold_val_logits.xlsx') as writer:
                 for k in range(3):
-                    model_ft = initialize_model(
-                        num_classes=number_classes,
-                        feature_extract=True, 
-                        use_pretrained=True
-                    )
-
-                    path_weight = path_weight_save + f"resnet_34_kfold_{str(k)}.pth"
-                    
-                    model_ft.load_state_dict(
-                        torch.load(
-                            path_weight,
-                            map_location=torch.device(device=device)
-                        )
-                    )
-                    model_ft.eval()
-                    model_ft = model_ft.to(device)
-
-                    dataframe_logits = []
-                    for information in informations:
-                        domain_name = information["domain_name"]
-                        if dataset_name ==  "Office-Home":
-                            path_root = f"/vinbrain/anhng/domain_adaptation/datasets/{dataset_name}/OfficeHomeDataset_10072016/{domain_name}/"
-                        else:
-                            path_root = f"/vinbrain/anhng/domain_adaptation/datasets/{dataset_name}/{domain_name}/images/"
-                        
-                        df_info_k_fold = information["dataframe"].copy()
-                        df_info_k_fold["domain_name"] = domain_name
-                        df_info_k_fold["logit"] = np.nan
-                        df_info_k_fold["predict"] = np.nan
-                        df_info_k_fold["phase"] = "train"
-                        df_info_k_fold.loc[df_info_k_fold.kfold==k, ['phase']] = 'val'
-                        
-                        image_datasets = {
-                            "train": Dataset(
-                                df_info=df_info_k_fold[df_info_k_fold.phase=="train"],
-                                folder_data=path_root,
-                                image_size=config_params["input_size"], 
-                                transforms=data_transforms["train"]),
-                            "val": Dataset(
-                                df_info=df_info_k_fold[df_info_k_fold.phase=="val"], 
-                                folder_data=path_root,
-                                image_size=config_params["input_size"], 
-                                transforms=data_transforms["val"])
-                        }
-
-                        dataloaders_dict = {
-                            x: torch.utils.data.DataLoader(
-                                image_datasets[x],
-                                collate_fn=default_collate,
-                                batch_size=config_params["batch_size"], 
-                                shuffle=True, 
-                                num_workers=config_params["num_workers"]) for x in ['train', 'val']}
-
-                        df_finish_pred = export_logits(
-                            model_ft, dataloaders_dict, 
-                            folder_save=path_weight_save,
-                            k_fold=k
+                    if k in config_params["kfold_exp"]:
+                        model_ft = initialize_model(
+                            num_classes=number_classes,
+                            feature_extract=True, 
+                            use_pretrained=True
                         )
 
-                        dataframe_logits.append(df_finish_pred)
-                    
-                    dataframe_logits = pd.concat(dataframe_logits)
-                    dataframe_logits.to_excel(writer, sheet_name=f'kfold_{k}')
+                        path_weight = path_weight_save + f"resnet_34_kfold_{str(k)}.pth"
+                        
+                        model_ft.load_state_dict(
+                            torch.load(
+                                path_weight,
+                                map_location=torch.device(device=device)
+                            )
+                        )
+                        model_ft.eval()
+                        model_ft = model_ft.to(device)
+
+                        dataframe_logits = []
+                        for information in informations:
+                            domain_name = information["domain_name"]
+                            if dataset_name ==  "Office-Home":
+                                path_root = f"{config_params['path_data']}/{dataset_name}/OfficeHomeDataset_10072016/{domain_name}/"
+                            elif dataset_name == "Bing-Caltech":
+                                path_root = f"{config_params['path_data']}/{dataset_name}/BingLarge_C256_deduped/"
+                            elif dataset_name == "Domain-net":
+                                path_root = f"{config_params['path_data']}/{dataset_name}/{domain_name}/"
+                            else:
+                                path_root = f"{config_params['path_data']}/{dataset_name}/{domain_name}/images/"
+                            
+                            df_info_k_fold = information["dataframe"].copy()
+                            df_info_k_fold["domain_name"] = domain_name
+                            df_info_k_fold["logit"] = np.nan
+                            df_info_k_fold["predict"] = np.nan
+                            df_info_k_fold["phase"] = "train"
+                            df_info_k_fold.loc[df_info_k_fold.kfold==k, ['phase']] = 'val'
+                            
+                            image_datasets = {
+                                "train": Dataset(
+                                    df_info=df_info_k_fold[df_info_k_fold.phase=="train"],
+                                    folder_data=path_root,
+                                    image_size_ratio=config_params["image_size_ratio"], 
+                                    input_size=config_params["input_size"], 
+                                    transforms=data_transforms["train"]),
+                                "val": Dataset(
+                                    df_info=df_info_k_fold[df_info_k_fold.phase=="val"], 
+                                    folder_data=path_root,
+                                    image_size_ratio=config_params["image_size_ratio"], 
+                                    input_size=config_params["input_size"],
+                                    transforms=data_transforms["val"]),
+                            }
+
+                            dataloaders_dict = {
+                                x: torch.utils.data.DataLoader(
+                                    image_datasets[x],
+                                    collate_fn=default_collate,
+                                    batch_size=config_params["batch_size"], 
+                                    shuffle=True, 
+                                    num_workers=config_params["num_workers"]) for x in ['train', 'val']}
+                            
+                            if main_domain == domain_name:
+                                export_main_domain = True
+                            else:
+                                export_main_domain = False
+                            
+                            df_finish_pred = export_logits(
+                                model_ft, dataloaders_dict, 
+                                export_main_domain=export_main_domain,
+                            )
+
+                            dataframe_logits.append(df_finish_pred)
+                        
+                        dataframe_logits = pd.concat(dataframe_logits)
+                        dataframe_logits.to_excel(writer, sheet_name=f'kfold_{k}')
