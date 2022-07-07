05236c8916eac52e0030fe95f1bff459514d5022
tuananh <tuananh14111998@gmail.com>
2022-07-07 18:23:08 +0700

    update compute_adaptation_calibration

--- src/metrics/get_metrics.py
+++ src/metrics/get_metrics.py
@@ -150,7 +150,30 @@ def compute_calibration(true_labels, pred_labels, confidences, num_bins=10):
     return ece
 
 
+# def compute_adaptation_calibration(true_labels, pred_labels, confidences, num_bins=10):
+#     bin_size = 1.0 / num_bins
+#     _, bins = pd.qcut(confidences, q = 10, retbins = True)
+#     indices = np.digitize(confidences, bins, right=True)
+#     bin_accuracies = np.zeros(num_bins, dtype=np.float)
+#     bin_confidences = np.zeros(num_bins, dtype=np.float)
+#     bin_counts = np.zeros(num_bins, dtype=np.int)
+#     for b in range(num_bins):
+#         selected = np.where(indices == b + 1)[0]
+#         if len(selected) > 0:
+#             bin_accuracies[b] = np.mean(true_labels[selected] == pred_labels[selected])
+#             bin_confidences[b] = np.mean(confidences[selected])
+#             bin_counts[b] = len(selected)
+#     avg_acc = np.sum(bin_accuracies * bin_counts) / np.sum(bin_counts)
+#     avg_conf = np.sum(bin_confidences * bin_counts) / np.sum(bin_counts)
+#     gaps = np.abs(bin_accuracies - bin_confidences)
+#     ece = np.sum(gaps * bin_counts) / np.sum(bin_counts)
+#     mce = np.max(gaps)
+#     return ece
+
 def compute_adaptation_calibration(true_labels, pred_labels, confidences, num_bins=10):
+    np.random.seed(0)
+    epsilon = np.random.rand(confidences.shape[0]) / 10**(9)
+    confidences += epsilon
     bin_size = 1.0 / num_bins
     _, bins = pd.qcut(confidences, q = 10, retbins = True)
     indices = np.digitize(confidences, bins, right=True)
@@ -168,6 +191,7 @@ def compute_adaptation_calibration(true_labels, pred_labels, confidences, num_bi
     gaps = np.abs(bin_accuracies - bin_confidences)
     ece = np.sum(gaps * bin_counts) / np.sum(bin_counts)
     mce = np.max(gaps)
+    
     return ece
 
 
